7966492f368545c055411143d16ea8c9b58b81c4
EC2 Default User <ec2-user@ip-172-31-60-122.ec2.internal>
2021-01-18 01:33:52 +0000

    second commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
 end
+
+Refile.secret_key = '8cd2f73fed424b42b5d99c748015daecb1013dc1f610ae709785bc6a674eb466cbcbcced1b1cfbbd1725733f62f47b86a72886a1212da500ecea84b02dc245a3'
